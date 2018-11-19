7f8227ca38c97a2f6c4ec4408dc42745f83a6763
Gaurav Vasudev <gaurav.nv1@gmail.com>
2018-11-19 13:30:04 -0800

    Updates

---
+++ precache-manifest.047946a5d53a48d05bab9ae7d6de8999.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "95c9596df0cfa5e42acaf5c02a1bad9d",
+    "url": "/fbank304/static/media/banner.95c9596d.png"
+  },
+  {
+    "revision": "4f33a27e9e77571489bf",
+    "url": "/fbank304/static/js/runtime~main.4f33a27e.js"
+  },
+  {
+    "revision": "139024bb1c4ec701e9aa",
+    "url": "/fbank304/static/js/main.139024bb.chunk.js"
+  },
+  {
+    "revision": "fdb84e7622f7fb95086d",
+    "url": "/fbank304/static/js/1.fdb84e76.chunk.js"
+  },
+  {
+    "revision": "139024bb1c4ec701e9aa",
+    "url": "/fbank304/static/css/main.a3a9f6af.chunk.css"
+  },
+  {
+    "revision": "fdb84e7622f7fb95086d",
+    "url": "/fbank304/static/css/1.8c23c235.chunk.css"
+  },
+  {
+    "revision": "9018222a0c3c5f215f03be224b640b22",
+    "url": "/fbank304/index.html"
+  }
+];
